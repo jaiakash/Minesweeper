53a1ac0be11188fb36920488d8de64d1b701cbd7
Akash Jaiswal <akashjaiswal3846@gmail.com>
2021-07-31 01:13:59 +0530

    Added click event to blocks.

--- app/src/main/java/com/amostrone/akash/minessweeper/Game.java
+++ app/src/main/java/com/amostrone/akash/minessweeper/Game.java
@@ -2,16 +2,25 @@ package com.amostrone.akash.minessweeper;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
+import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.Region;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
+
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 public class Game extends View {
 
@@ -20,6 +29,8 @@ public class Game extends View {
     Paint blockPaint = new Paint();
     Paint minePaint = new Paint();
     Paint safePaint = new Paint();
+
+    RectF blockArray[][] = new RectF[8][8];//Assume these have been drawn in your draw method.
 
     public Game(Context context) {
         super(context);
@@ -53,7 +64,12 @@ public class Game extends View {
         int screenWidth = displayMetrics.widthPixels;
         int screenHeight = displayMetrics.heightPixels;
 
-
+        int padding=250;
+        for(int i=1;i<=8;i++) {
+            for (int j = 0; j < 8; j++) {
+                blockArray[i-1][j] = new RectF(j * 125 + 40, i*150+padding, j * 125 + 125, i*150+100+padding);
+            }
+        }
     }
 
     @Override
@@ -62,10 +78,9 @@ public class Game extends View {
 
         canvas.drawPaint(backgroundPaint);
 
-        int padding=250;
         for(int i=1;i<=8;i++) {
             for (int j = 0; j < 8; j++) {
-                canvas.drawRoundRect(j * 125 + 40, i*150+padding, j * 125 + 125, i*150+100+padding, 10, 10, blockPaint);
+                canvas.drawRect(blockArray[i-1][j],blockPaint);
             }
         }
 
@@ -78,6 +93,30 @@ public class Game extends View {
 
     public static float pxFromDp(final Context context, final float dp) {
         return dp * context.getResources().getDisplayMetrics().density;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int touchX = (int) event.getX();
+        int touchY = (int) event.getY();
+        switch(event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+
+                for(int i=1;i<=8;i++) {
+                    for (int j = 0; j < 8; j++) {
+                        if(blockArray[i-1][j].contains(touchX,touchY)) {
+                            Log.i("Clicked", "Clicked Block i="+(i-1)+" j="+j);
+                        }
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                System.out.println("Sliding your finger around on the screen.");
+                break;
+        }
+        return true;
     }
 
 }
