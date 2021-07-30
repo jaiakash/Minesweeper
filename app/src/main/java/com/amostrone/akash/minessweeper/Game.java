a11b6819c1729ec11d5601106f60776f7d96ae27
Akash Jaiswal <akashjaiswal3846@gmail.com>
2021-07-30 22:29:58 +0530

    Made background

--- app/src/main/java/com/amostrone/akash/minessweeper/Game.java
+++ app/src/main/java/com/amostrone/akash/minessweeper/Game.java
@@ -18,11 +18,6 @@ public class Game extends View {
     RectF mRectF;
     int mPadding;
 
-    float arcLeft, arcTop, arcRight, arcBottom;
-
-    Path mPath;
-
-
     public Game(Context context) {
         super(context);
 
@@ -33,19 +28,16 @@ public class Game extends View {
         mPaint.setColor(Color.BLUE);
         mPaint.setStrokeWidth(5);
 
-
         mTextPaint = new Paint(Paint.LINEAR_TEXT_FLAG | Paint.ANTI_ALIAS_FLAG);
         mTextPaint.setColor(Color.BLACK);
         mTextPaint.setTextSize(pxFromDp(context, 24));
 
         otherPaint = new Paint();
-
         outerPaint = new Paint();
         outerPaint.setStyle(Paint.Style.FILL);
         outerPaint.setColor(Color.YELLOW);
 
         mPadding = 100;
-
 
         DisplayMetrics displayMetrics = new DisplayMetrics();
 
@@ -57,23 +49,7 @@ public class Game extends View {
         int screenWidth = displayMetrics.widthPixels;
         int screenHeight = displayMetrics.heightPixels;
 
-        arcLeft = pxFromDp(context, 20);
-        arcTop = pxFromDp(context, 20);
-        arcRight = pxFromDp(context, 100);
-        arcBottom = pxFromDp(context, 100);
-
-
-        Point p1 = new Point((int) pxFromDp(context, 80) + (screenWidth / 2), (int) pxFromDp(context, 40));
-        Point p2 = new Point((int) pxFromDp(context, 40) + (screenWidth / 2), (int) pxFromDp(context, 80));
-        Point p3 = new Point((int) pxFromDp(context, 120) + (screenWidth / 2), (int) pxFromDp(context, 80));
-
-        mPath = new Path();
-        mPath.moveTo(p1.x, p1.y);
-        mPath.lineTo(p2.x, p2.y);
-        mPath.lineTo(p3.x, p3.y);
-        mPath.close();
-
-        mRectF = new RectF(screenWidth / 4, screenHeight / 3, screenWidth / 6, screenHeight / 2);
+        mRectF = new RectF(screenWidth / 4, screenHeight / 4, screenWidth / 6, screenHeight /6);
 
     }
 
@@ -81,30 +57,18 @@ public class Game extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        otherPaint.setColor(Color.RED);
+        otherPaint.setStyle(Paint.Style.FILL);
+
         canvas.drawRoundRect(mRectF, 10, 10, otherPaint);
         canvas.clipRect(mRectF, Region.Op.DIFFERENCE);
         canvas.drawPaint(outerPaint);
 
-        canvas.drawLine(250, 250, 400, 400, mPaint);
-        canvas.drawRect(mPadding, mPadding, getWidth() - mPadding, getHeight() - mPadding, mPaint);
-        canvas.drawArc(arcLeft, arcTop, arcRight, arcBottom, 75, 45, true, mPaint);
+        canvas.drawRect(0,0,50,50,outerPaint);
 
+        canvas.drawRect(mPadding, mPadding+150, getWidth() - mPadding, getHeight() - mPadding, mPaint);
 
-        otherPaint.setColor(Color.GREEN);
-        otherPaint.setStyle(Paint.Style.FILL);
-
-        canvas.drawRect(
-                getLeft() + (getRight() - getLeft()) / 3,
-                getTop() + (getBottom() - getTop()) / 3,
-                getRight() - (getRight() - getLeft()) / 3,
-                getBottom() - (getBottom() - getTop()) / 3, otherPaint);
-
-
-        canvas.drawPath(mPath, mPaint);
-        otherPaint.setColor(Color.BLACK);
-        canvas.drawCircle(getWidth() / 2, getHeight() / 2, arcLeft, otherPaint);
-
-        canvas.drawText("Canvas basics", (float) (getWidth() * 0.3), (float) (getHeight() * 0.8), mTextPaint);
+        canvas.drawText("Minesweeper", (float) (getWidth() * 0.3), (float) (getHeight() * 0.8), mTextPaint);
 
     }
 
