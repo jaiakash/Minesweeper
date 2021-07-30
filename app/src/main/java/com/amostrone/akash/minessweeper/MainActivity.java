0c0eb1477e6f5055b41a3d0123f9f0ddd71d52ce
Akash Jaiswal <akashjaiswal3846@gmail.com>
2021-07-30 21:55:42 +0530

    Added canvas class

--- app/src/main/java/com/amostrone/akash/minessweeper/MainActivity.java
+++ app/src/main/java/com/amostrone/akash/minessweeper/MainActivity.java
@@ -1,14 +1,21 @@
 package com.amostrone.akash.minessweeper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.LinearLayout;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    LinearLayout linearLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        linearLayout = findViewById(R.id.linearLayout);
+        Game gameView = new Game(this);
+        linearLayout.addView(gameView);
     }
 }
